89824c7c7fcb8d2a28bf83afdb6adfa22d9ef8a0
RicoZethof <ricozethof@gmail.com>
2020-07-23 10:06:13 +0200

    RZ - attach-camera.js -> Gives each player his own camera

--- CM3D/Scripts/attach-camera.js
+++ CM3D/Scripts/attach-camera.js
@@ -1,52 +1,59 @@
-let attachCamInterval;
 class attachCamera {
-  constructor() {}
+  constructor() {
+    this.attachCamInterval = null;
+  }
   attach() {
+    // Variables
+    let myCam;
     const camHash = "vide";
-    const camEl = getFirstElementFromHash(camHash);
-
-    if (camEl) {
-      attachCamInterval = setInterval(attachCam, 10);
-    } else {
-      console.warn("You need to active your webcam first");
-    }
-
-    camEl.object3D.scale.setScalar(0.3);
     const selfEl = AFRAME.scenes[0].querySelector("#avatar-rig");
-    const povCam = selfEl.querySelector(".model");
-    var headElrotation = new THREE.Quaternion();
+    const headElrotation = new THREE.Quaternion();
     const headEl = {
       position: new THREE.Vector3()
     };
-
-    function attachCam() {
-      attachObjToAvatar(camEl, selfEl);
-    }
-
-    function attachObjToAvatar(obj, avatar) {
-      NAF.utils.getNetworkedEntity(obj).then(networkedEl => {
-        const mine = NAF.utils.isMine(networkedEl);
-        if (!mine) var owned = NAF.utils.takeOwnership(networkedEl);
-        avatar.querySelectorAll("#avatar-head")[0].object3D.getWorldPosition(headEl.position);
-        networkedEl.object3D.position.copy(headEl.position);
-        networkedEl.object3D.position.y += 0.05;
-        networkedEl.object3D.setRotationFromQuaternion(avatar.querySelectorAll("#avatar-head")[0].object3D.getWorldQuaternion(headElrotation));
-      });
-    }
-
-    function getFirstElementFromHash(hash) {
+    // Get all the camera elements
+    function getAllElementsFromHash(hash) {
       const g = AFRAME.scenes[0].querySelectorAll("[media-loader]");
       const matches = [];
       for (const e of g) {
         const m = e.components["media-loader"].attrValue.src.match(hash);
         if (m && m.length) matches.push(e);
       }
-      return matches[0];
+      return matches;
+    }
+    // Attach your camera to your avatar
+    function attachObjToAvatar(obj, avatar) {
+      NAF.utils.getNetworkedEntity(obj).then(networkedEl => {
+        console.log("I AM RUNNING");
+        avatar.querySelectorAll("#avatar-head")[0].object3D.getWorldPosition(headEl.position);
+        networkedEl.object3D.position.copy(headEl.position);
+        networkedEl.object3D.position.y += 0.05;
+        networkedEl.object3D.setRotationFromQuaternion(
+          avatar.querySelectorAll("#avatar-head")[0].object3D.getWorldQuaternion(headElrotation)
+        );
+      });
+    }
+    // Runs on setInterval every ... frames
+    function attachCam() {
+      attachObjToAvatar(myCam, selfEl);
+    }
+    const camEl = getAllElementsFromHash(camHash);
+    if (!camEl) return;
+    // Finding YOUR camera to put on the avatar
+    for (const cam of camEl) {
+      NAF.utils.getNetworkedEntity(cam).then(networkedEl => {
+        const mine = NAF.utils.isMine(networkedEl);
+        if (mine) {
+          myCam = cam;
+          this.attachCamInterval = setInterval(attachCam, 35);
+          cam.object3D.scale.setScalar(0.3);
+        }
+      });
     }
   }
 
   detach() {
-    attachCamInterval = clearInterval();
+    this.attachCamInterval = clearInterval();
   }
 }
 
