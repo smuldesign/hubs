3c858c44fdb0c2b515a9c335f6688d75efd81810
Rico Zethof <Ricozethof@gmail.com>
2020-08-25 14:03:22 +0200

    Revert "Rz - Feedback testronde hva"
    This reverts commit 184e2e970cbf58e086c81a200f5c4e566f669c5f.

--- src/react-components/avatar-editor.js
+++ src/react-components/avatar-editor.js
@@ -187,29 +187,29 @@ export default class AvatarEditor extends Component {
         <div className="img-box" />
         <span>{label}</span>
       </label>
-      {/*<input*/}
-      {/*  id={`avatar-file_${name}`}*/}
-      {/*  type="file"*/}
-      {/*  accept={accept}*/}
-      {/*  disabled={disabled}*/}
-      {/*  onChange={e => {*/}
-      {/*    const file = e.target.files[0];*/}
-      {/*    e.target.value = null;*/}
-      {/*    this.inputFiles[name] = file;*/}
-      {/*    URL.revokeObjectURL(this.state.previewGltfUrl);*/}
-      {/*    const previewGltfUrl = URL.createObjectURL(this.inputFiles.glb);*/}
-      {/*    this.setState({*/}
-      {/*      previewGltfUrl,*/}
-      {/*      avatar: {*/}
-      {/*        ...this.state.avatar,*/}
-      {/*        files: {*/}
-      {/*          ...this.state.avatar.files,*/}
-      {/*          [name]: URL.createObjectURL(file)*/}
-      {/*        }*/}
-      {/*      }*/}
-      {/*    });*/}
-      {/*  }}*/}
-      {/*/>*/}
+      <input
+        id={`avatar-file_${name}`}
+        type="file"
+        accept={accept}
+        disabled={disabled}
+        onChange={e => {
+          const file = e.target.files[0];
+          e.target.value = null;
+          this.inputFiles[name] = file;
+          URL.revokeObjectURL(this.state.previewGltfUrl);
+          const previewGltfUrl = URL.createObjectURL(this.inputFiles.glb);
+          this.setState({
+            previewGltfUrl,
+            avatar: {
+              ...this.state.avatar,
+              files: {
+                ...this.state.avatar.files,
+                [name]: URL.createObjectURL(file)
+              }
+            }
+          });
+        }}
+      />
       {this.state.avatar.files[name] && (
         <a
           onClick={() => {
